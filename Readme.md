210669c5db94ce243cd9f714430f09893790842b
Jan-Niclas Struewer <j.n.struewer@gmail.com>
2022-04-20 15:53:22 +0200

    additional queries

--- Readme.md
+++ Readme.md
@@ -1,6 +1,10 @@
 # Swift and Objective-C Counter
 This code prints the number of Objective-C and Swift repositories on GitHub.
 For the 19th of April there are a total count of 534489 Objective-C repositories and 986992 Swift repositories.
+Additionally, 3752 Swift repositories are found with the `server` tag. 
+Before the introduction of Swift in 2014 there had been 77210 Objective-C repositories.
+
+These numbers might decrease if repositories get deleted.
 
 To get the code to produce more current data alter the `createdBefore` constant in the `main.ts` file.
 
--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,32 @@ import {Octokit} from 'octokit';
 
 const octokit = new Octokit({});
 
-const queryNumberOfRepos = (language: string, created: string) => {
-  const searchQuery = {q: `language:${language} created:<${created}`};
+const queryNumberOfRepos = async (
+  language: string,
+  created: string,
+  textSearch?: string
+) => {
+  const search = textSearch ? textSearch + '+' : '';
+  const searchQuery = {q: `${search}language:${language} created:<${created}`};
+
   console.debug(
     `Starting query for ${language} repositories, which were created before ${created}`
   );
   octokit.request('GET /search/repositories', searchQuery).then(response => {
     if (response.data.incomplete_results) {
-      console.warn(`Results for ${language} query are incomplete`);
+      console.warn(`Results for ${JSON.stringify(searchQuery)} are incomplete`);
     }
     console.log(
-      `Total count ${language} language repos ${response.data.total_count}`
+      `Total count for query ${JSON.stringify(searchQuery)} ${
+        response.data.total_count
+      }`
     );
   });
 };
+
 const createdBefore = '2022-04-19';
 queryNumberOfRepos('Objective-C', createdBefore);
+const releaseDateSwift = '2014-01-01';
+queryNumberOfRepos('Objective-C', releaseDateSwift);
 queryNumberOfRepos('swift', createdBefore);
+queryNumberOfRepos('swift', createdBefore, 'server');
