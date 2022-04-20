eb497924934b4d29eaf13c3a30146d1877412fb7
Jan-Niclas Struewer <j.n.struewer@gmail.com>
2022-04-20 14:23:26 +0200

    printing number of swift and objective-c repositories at the 19.04.2022

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,21 @@
-console.log('hello world');
+import {Octokit} from 'octokit';
+
+const octokit = new Octokit({});
+
+const queryNumberOfRepos = (language: string, created: string) => {
+  const searchQuery = {q: `language:${language} created:<${created}`};
+  console.debug(
+    `Starting query for ${language} repositories, which were created before ${created}`
+  );
+  octokit.request('GET /search/repositories', searchQuery).then(response => {
+    if (response.data.incomplete_results) {
+      console.warn(`Results for ${language} query are incomplete`);
+    }
+    console.log(
+      `Total count ${language} language repos ${response.data.total_count}`
+    );
+  });
+};
+const createdBefore = '2022-04-19';
+queryNumberOfRepos('Objective-C', createdBefore);
+queryNumberOfRepos('swift', createdBefore);
